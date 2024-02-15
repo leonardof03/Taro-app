004feb429ae29e37110a7eab9caa0efba47e5b5a
Leonardo Ferreira <leonardoaaaaraujo@gmail.com>
2024-02-15 11:08:57 +0000

    Tentativa de layout melhorado

--- src/App.js
+++ src/App.js
@@ -1,58 +1,36 @@
-// App.js
 import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 import tarotDeck from './tarotDeck';
 
-
-
 function App() {
-  const [selectedCard, setSelectedCard] = useState(null);
-  const [reading, setReading] = useState("");
-  const [readingHistory, setReadingHistory] = useState([]);
   const [readingType, setReadingType] = useState('Amor e Relacionamentos');
   const [layoutType, setLayoutType] = useState('Leitura Três Cartas');
   const [isLoading, setIsLoading] = useState(false);
+  const [userQuestion, setUserQuestion] = useState("");
+  const [selectedCard, setSelectedCard] = useState(null);
+  const [reading, setReading] = useState("");
+  const [readingHistory, setReadingHistory] = useState([]);
 
-
-  // Defina as variáveis para os detalhes da carta.
-  const [showCardDetails, setShowCardDetails] = useState(false);
-  const [cardDetails, setCardDetails] = useState({ name: '', description: '', meaning: '' });
-
-  // Função para selecionar uma carta
-  const selectCard = (cardKey) => {
-    setSelectedCard(cardKey);
-    // Aqui você pode definir as informações detalhadas da carta com base na carta selecionada.
-    setCardDetails(tarotDeck[cardKey]);
-    setShowCardDetails(true); // Mostra os detalhes da carta.
+  const selectRandomCard = () => {
+    const cardKeys = Object.keys(tarotDeck);
+    const randomIndex = Math.floor(Math.random() * cardKeys.length);
+    const randomCardKey = cardKeys[randomIndex];
+    return randomCardKey;
   };
-
-  // Função para renderizar os detalhes da carta
-  const renderCardDetails = () => {
-    if (!showCardDetails) return null;
-
-    return (
-      <div className="card-details">
-        <h3>{cardDetails.name}</h3>
-        <p>{cardDetails.description}</p>
-        <p>{cardDetails.meaning}</p>
-        {/* Adicione um botão para ocultar os detalhes da carta. */}
-        <button onClick={() => setShowCardDetails(false)}>Fechar Detalhes</button>
-      </div>
-    );
-  };
-
 
   const getTarotReading = async () => {
-    if (!selectedCard) {
-      alert("Por favor, selecione uma carta primeiro.");
+    if (!userQuestion) {
+      alert("Por favor, digite sua pergunta personalizada.");
       return;
     }
+
     setIsLoading(true);
-    const cardDescription = tarotDeck[selectedCard].description;
-    const prompt = `Realiza uma leitura de Tarô no tema '${readingType}' utilizando o layout '${layoutType}' para a carta com a seguinte descrição: '${cardDescription}'. Por favor, forneça orientações e insights sobre o assunto que possam ser úteis e esclarecedores para o cliente.`;
 
     try {
+      const selectedCardKey = selectRandomCard();
+      setSelectedCard(selectedCardKey);
+
       const response = await axios.post('https://api.openai.com/v1/chat/completions', {
         model: "gpt-3.5-turbo",
         messages: [{
@@ -60,7 +38,7 @@ function App() {
           content: "Bem-vindo à Plataforma de Tarô! Estás a conversar com um leitor de tarô. Sinta-te à vontade para fazer a tua pergunta ou escolher uma leitura específica. Estou aqui para ajudar."
         }, {
           role: "user",
-          content: prompt
+          content: `Realiza uma leitura de Tarô no tema '${readingType}' utilizando o layout '${layoutType}' para a carta "${tarotDeck[selectedCardKey]?.name}" com a seguinte descrição: '${tarotDeck[selectedCardKey]?.description}'. Por favor, forneça orientações e insights sobre o assunto que possam ser úteis e esclarecedores para o cliente. Pergunta do usuário: '${userQuestion}'`
         }]
       }, {
         headers: {
@@ -79,44 +57,73 @@ function App() {
     }
   };
 
-
+  const resetApp = () => {
+    setSelectedCard(null);
+    setReading("");
+    setUserQuestion("");
+    setIsLoading(false);
+    setReadingType('Amor e Relacionamentos');
+    setLayoutType('Leitura Três Cartas');
+  };
   return (
-    <div className="App">
+    <div className="tarot-container">
       <header className="App-header">
-        <p>Bem-vindo à Plataforma de Tarô</p>
-        <div className="tarot-cards" >
-          {Object.keys(tarotDeck).map((cardKey, index) => (
-            <img
-              key={index}
-              src={`/images/${cardKey}`}
-              alt={tarotDeck[cardKey].name}
-              className={`card ${selectedCard === cardKey ? 'card-selected' : ''} card-enter-animation`}
-              onClick={() => selectCard(cardKey)}
+        <div className="top-bar">
+          <p>Bem-vindo à Plataforma de Tarô do Leonardinho</p>
+          <div className="input-container">
+            <textarea
+              value={userQuestion}
+              onChange={(e) => setUserQuestion(e.target.value)}
+              placeholder="Digite sua pergunta personalizada"
             />
-          ))}
-          {isLoading && <p>Carregando...</p>}
+            <div className="button-group">
+              <button onClick={getTarotReading}>Obter Leitura de Tarô</button>
+              <button onClick={resetApp}>Clear</button>
+            </div>
+          </div>
+          <div className="select-container">
+            <select value={readingType} onChange={e => setReadingType(e.target.value)}>
+              <option value="Amor e Relacionamentos">Amor e Relacionamentos</option>
+              <option value="Carreira e Finanças">Carreira e Finanças</option>
+              <option value="Saúde e Bem-Estar">Saúde e Bem-Estar</option>
+              <option value="Espiritual">Espiritual</option>
+              <option value="Decisões e Escolhas">Decisões e Escolhas</option>
+              <option value="Família e Amigos">Família e Amigos</option>
+            </select>
+            <select value={layoutType} onChange={e => setLayoutType(e.target.value)}>
+              <option value="Leitura Três Cartas">Leitura Três Cartas</option>
+              <option value="Leitura Cruz Celta">Leitura Cruz Celta</option>
+              <option value="Leitura Uma Carta">Leitura Uma Carta</option>
+              <option value="Leitura Sim/Não">Leitura Sim/Não</option>
+            </select>
+          </div>
+        </div>
   
+        <div className="tarot-cards">
+          {isLoading ? (
+            <div className="tarot-loading-animation">
+              <img src="/images/tarot.gif" alt="Baralho de cartas de tarô girando" />
+            </div>
+            
+          ) : (
+            <div className="card-container"> {/* Novo contêiner para a carta de tarô */}
+              <img
+                src={`/images/${selectedCard}`}
+                alt={tarotDeck[selectedCard]?.name}
+                className={`card card-selected`}
+              />
+            </div>
+          )}
+          {!isLoading && selectedCard && (
+            <div className="card-details">
+              <h3>{tarotDeck[selectedCard]?.name}</h3>
+              <p>{tarotDeck[selectedCard]?.description}</p>
+              <p>{tarotDeck[selectedCard]?.meaning}</p>
+            </div>
+          )}
           {!isLoading && reading && <p className="reading">{reading}</p>}
         </div>
-        <select value={readingType} onChange={e => setReadingType(e.target.value)}>
-          <option value="Amor e Relacionamentos">Amor e Relacionamentos</option>
-          <option value="Carreira e Finanças">Carreira e Finanças</option>
-          <option value="Saúde e Bem-Estar">Saúde e Bem-Estar</option>
-          <option value="Espiritual">Espiritual</option>
-          <option value="Decisões e Escolhas">Decisões e Escolhas</option>
-          <option value="Família e Amigos">Família e Amigos</option>
-        </select>
-
-        <select value={layoutType} onChange={e => setLayoutType(e.target.value)}>
-          <option value="Leitura Três Cartas">Leitura Três Cartas</option>
-          <option value="Leitura Cruz Celta">Leitura Cruz Celta</option>
-          <option value="Leitura Uma Carta">Leitura Uma Carta</option>
-          <option value="Leitura Sim/Não">Leitura Sim/Não</option>
-        </select>
-        <button onClick={getTarotReading}>Obter Leitura de Tarô</button>
-
-        {renderCardDetails()} { }
-
+  
         <div className="reading-history">
           <h3>Histórico de Leituras</h3>
           <ul>
@@ -128,6 +135,8 @@ function App() {
       </header>
     </div>
   );
+  
+  
 }
 
 export default App;
